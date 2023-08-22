d1ae2e8f5daf7e56978cb0ca95408c591e5b44d8
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-21 20:00:44 -0700

    setting up images and tailwind add ons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  }),]
+});
