d5b3b676db5926f78a55bf90f2ac5ba804c1e7e7
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-21 20:31:22 -0700

    adding solid.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-  }),]
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), solidJs()]
 });
